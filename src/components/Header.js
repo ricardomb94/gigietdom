838cb351376cb60dbf6624b4da3fb61599953c42
ricardomb94 <aymardmb@gmail.com>
2023-02-28 18:57:51 +0100

    Setup image using backgroundImage property

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,34 @@ import { Image } from 'mui-image';
 import imageUrl from '../assets/images/mariage-large.jpg';
 import { Slide } from "react-awesome-reveal";
 
+const styles = {
+    heroContainer: {
+        height: 800,
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: `calc(100vw + 48px)`,
+        margin: -24,
+        padding: 24,
+    }
+};
 
 const Header = () => {
     return (
 
-        <Grid sx={{ position: 'relative', textAlign: 'center' }} >
-            <Image src={imageUrl} alt="unis pour la vie" sx={{ width: '100%', height: '50vh' }} />
+        <Grid
+            style={styles.heroContainer}
+            sx={{ position: 'relative', textAlign: 'center' }}
+        >
+            {/* <Image src={imageUrl} alt="unis pour la vie" sx={{ width: '100%', height: '50vh' }} /> */}
             <Slide cascade damping={0.1}>
-                <Typography variant="h3" sx={{ padding: '0.5rem' }}>Ogilvy et Dominique  </Typography>
-                <hr />
-                <Typography variant="h5">
-                    16 Septembre 2023
-                </Typography>
+                <Grid item>
+                    <Typography variant="h3" sx={{ padding: '0.5rem' }}>Ogilvy et Dominique  </Typography>
+                    <hr />
+                    <Typography variant="h5">
+                        16 Septembre 2023
+                    </Typography>
+                </Grid>
             </Slide>
         </Grid>
     )
