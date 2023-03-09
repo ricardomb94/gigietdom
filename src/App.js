f648c1a6053035d7a7d3e5f39f2f6671a904bdbf
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2023-03-09 03:16:02 +0100

    Adding navigation with success

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Navbar from './components/Navbar'
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Ceremony from "./pages/Ceremony";
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { Box } from '@mui/material';
-
-// const LinksArray = ["Accueil", "Notre histoire", "Ceremonie", "Localisation"]
 
 const App = () => {
     return (
-        <Box className='App'>
-            <Navbar />
-            <Header />
-        </Box>
+            <Router>
+                <Navbar />
+                <Header />
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/ceremony" element={<Ceremony/>}/>
+                </Routes>
+            </Router>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { AppBar, Grid, IconButton, Tab, Tabs, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, Grid, IconButton, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { React, useState } from 'react';
 import { Image } from 'mui-image';
@@ -7,6 +7,7 @@ import logo from '../assets/images/bague.png'
 import styled from '@emotion/styled';
 import { Favorite } from '@mui/icons-material';
 import { pink } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
 
 
 // const StyledTabs = styled(Tabs)(({ theme }) => ({
@@ -16,6 +17,7 @@ import { pink } from '@mui/material/colors';
 //     }
 // }))
 
+const navItems = [{path:"/", label:"Accueil"}, {path:"/About", label:"Notre histoire"}, {path:"/Ceremony", label:"Cérémonie"}]
 
 const Navbar = ({ links }) => {
     return (
@@ -25,7 +27,31 @@ const Navbar = ({ links }) => {
                     <IconButton edge="start" color="inherit" aria-label="menu" >
                         <MenuIcon sx={{backgroundColor: 'black', display:{sm:'none'}}}/>
                     </IconButton>
-                   
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, color: '#000000' }}
+                    >
+                      GIGI&DOM
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        {navItems.map((navItem) => (
+                        <Link to={navItem.path}
+                            key={navItem.path} 
+                            // sx={{ color: '#000000' }}
+                            underline="none"
+                            style={{ 
+                                marginLeft:20,
+                                color: '#000000',
+                                textDecoration:"none",
+                                textTransform: "uppercase"
+                            }}
+                          >
+                            {navItem.label}
+                        </Link>
+                     ))
+                    }
+                    </Box>  
                 </Toolbar>
             </Slide>
         </AppBar>
