87f0a6ca5f9c1b15c64dfe11dd643bd867390677
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2023-03-09 01:52:30 +0100

    Clean up the Navbar code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react'
 // import ImageCard from './components/ImageCard'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Ceremony from "./pages/Ceremony";
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const LinksArray = ["Accueil", "Notre histoire", "Ceremonie", "Localisation"]
+// const LinksArray = ["Accueil", "Notre histoire", "Ceremonie", "Localisation"]
 
 const App = () => {
     return (
-        <div className='App'>
-            <Navbar links={LinksArray} />
+        <Box className='App'>
+            <Navbar />
             <Header />
-        </div>
+        </Box>
     )
 }
 
